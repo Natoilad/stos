4b5b8cbde9ace653b1039a5d4018b3c94d05c6b0
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-09-28 04:35:29 +0300

    init

---
+++ js/totop.js
@@ -0,0 +1,18 @@
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Показуємо або ховаємо кнопку
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    scrollToTopBtn.style.display = "flex";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Функція для скролінгу нагору
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
