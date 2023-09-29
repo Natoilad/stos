15ba44da22cad79553256326178c1e1c247f1994
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-09-29 22:28:45 +0300

    re-fix-loader

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,15 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    mobMenu: document.querySelector('.mobile-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-    document.body.classList.toggle('modal-open'); 
+  function toggleModal() {
+    refs.mobMenu.style.display = 'block';
+    document.body.classList.toggle('modal-open');
     refs.menu.classList.toggle('is-open');
   }
 })();
