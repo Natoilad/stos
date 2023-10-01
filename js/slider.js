e71a1494a1b6b34205a7a5c8bbd7276f8d5b95de
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-01 05:11:57 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,42 @@
+// $(document).ready(function () {
+//   $('.testimonials').slick({
+//     slidesToShow: 3.5,
+//     slidesToScroll: 1,
+//     //   speed: 2000,
+//     //   dots: true,
+//     centerMode: true,
+//     centerPadding: '0px',
+//     // autoplay: true,
+//     //   autoplaySpeed: 2000,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           // dots: true,
+//           arrows: false,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+$(document).ready(function () {
+  $('.testimonials').slick({
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    centerMode: false, // Це змінено на false
+    infinite: false, // Це додано
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+    ],
+  });
+});
