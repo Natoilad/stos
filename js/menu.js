0b6a739e9a278abbcb0084742b04c5a337eb38a5
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-01 05:17:52 +0300

    fix-mob-menu

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,12 @@
     menu: document.querySelector('[data-menu]'),
     mobMenu: document.querySelector('.mobile-menu'),
   };
+  refs.mobMenu.style.display = 'block';
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.mobMenu.style.display = 'block';
     document.body.classList.toggle('modal-open');
     refs.menu.classList.toggle('is-open');
   }
