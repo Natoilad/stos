4bcf5708f4e675d91d3ad8985d731d5cb18e5895
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-04 17:47:21 +0300

    blur

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,7 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     mobMenu: document.querySelector('.mobile-menu'),
+    header: document.querySelector('.studio-header'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -18,7 +19,40 @@
       });
     }
     refs.mobMenu.style.opacity = '1';
+
+    // Set the backdrop-filter property based on the presence of the is-open class
+    refs.header.style.backdropFilter = refs.menu.classList.contains('is-open')
+      ? 'blur(5px)'
+      : 'none';
+
     document.body.classList.toggle('modal-open');
     refs.menu.classList.toggle('is-open');
   }
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//     mobMenu: document.querySelector('.mobile-menu'),
+//     header: document.querySelector('.studio-header'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     if (refs.menu) {
+//       refs.menu.addEventListener('click', event => {
+//         if (event.target.classList.contains('mobile-menu__link')) {
+//           refs.menu.classList.remove('is-open');
+//         }
+//       });
+//     }
+//     refs.mobMenu.style.opacity = '1';
+//     refs.header.style.backdropFilter = 'none';
+//     document.body.classList.toggle('modal-open');
+//     refs.menu.classList.toggle('is-open');
+//   }
+// })();
