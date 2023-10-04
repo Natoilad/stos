0c84e2854e09a436fc3c28aa6f681774f7d02871
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-04 21:03:10 +0300

    start_slider

---
+++ js/usa_slider.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const mainSlider = new Swiper('.main-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  const subSliders = document.querySelectorAll('.sub-slider');
+
+  subSliders.forEach(subSlider => {
+    new Swiper(subSlider, {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
+});
