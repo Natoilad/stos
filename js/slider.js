713682189267c0584e4dc5713a802b05ae575253
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-11 02:32:56 +0300

    seo

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,3 @@
-// $(document).ready(function () {
-//   $('.testimonials').slick({
-//     slidesToShow: 3.5,
-//     slidesToScroll: 1,
-//     //   speed: 2000,
-//     //   dots: true,
-//     centerMode: true,
-//     centerPadding: '0px',
-//     // autoplay: true,
-//     //   autoplaySpeed: 2000,
-//     responsive: [
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//           // dots: true,
-//           arrows: false,
-//         },
-//       },
-//     ],
-//   });
-// });
-
 $(document).ready(function () {
   $('.testimonials').slick({
     slidesToShow: 3,
