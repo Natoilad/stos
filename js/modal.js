589861b660c20dedfee914a1257658fa3cc744a3
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-10-12 04:06:16 +0300

    modal_init

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,39 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtns: document.querySelectorAll('[data-modal-open]'), // вибрати усі кнопки
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+  });
+  refs.modal.addEventListener('click', function (event) {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  });
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
--- sender.php
+++ sender.php
@@ -1,20 +1,18 @@
 <?php
     $name = $_POST['user-name'];
     $tel = $_POST['user-phone'];
-	$email = $_POST['user-email'];
     $text = $_POST['user-comment'];
 
 	$to = "living_beer@ukr.net"; 
 	$date = date ("d.m.Y"); 
 	$time = date ("h:i");
 	$from = $email;
-	$subject = "Заявка c сайта";
+	$subject = "Заявка з сайту";
 
 	
 	$msg="
     Ім'я: $name 
     Телефон: $tel 
-    Пошта: $email /n
     Текст: $text"; 	
 	mail($to, $subject, $msg, "From: $to ");
 
